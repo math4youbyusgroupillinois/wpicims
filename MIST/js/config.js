78cafce9b55c53b4fce771f5db69ca862b666165
Ctr for Industrial Math <cims@CCCWORK3.WPI.EDU>
2014-06-21 10:25:43 -0400

    committing for first time in months...

---
+++ MIST/js/config.js
@@ -0,0 +1,20 @@
+//<![CDATA[
+	  window.addEvent('domready', function(){
+	    var data = {
+	      '1.jpg': { }, 
+	      '2.jpg': { }, 
+	      '3.jpg': { }, 
+	      '4.jpg': { },
+	      '5.jpg': { },
+	      '6.jpg': { },
+	      '7.jpg': { }, 
+	      '8.jpg': { }, 
+	      '9.jpg': { },
+	      '10.jpg': { },
+	      '11.jpg': { }
+
+
+	    };
+	    var myShow = new Slideshow.KenBurns('kenburns_slideshow', data, { captions: false, controller: false, delay: 4000, duration: 1000, height: 240, hu: 'images/', width: 360 });
+		});
+	//]]>
